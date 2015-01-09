43016dcd6559f1441f4913f013a074885ba97cf4
Damian Reeves <damreev@gmail.com>
2015-01-09 17:07:19 -0500

    Working SSIS Project

--- src/SqlServer.Management.IntegrationServices/Script.fsx
+++ src/SqlServer.Management.IntegrationServices/Script.fsx
@@ -1,7 +1,7 @@
 // Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library.fs"
+#load "DataAccess.fs"
 open SqlServer.Management.IntegrationServices
 
 let num = Library.hello 42
