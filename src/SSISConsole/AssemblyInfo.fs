a54919a860d5c4afdaa347ffbad3b68d26f65dbf
Damian Reeves <damreev@gmail.com>
2015-01-14 16:58:07 -0500

    remove deployables from checkin

--- src/SSISConsole/AssemblyInfo.fs
+++ src/SSISConsole/AssemblyInfo.fs
@@ -3,10 +3,10 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("SSISConsole")>]
 [<assembly: AssemblyProductAttribute("SqlServer.Management.IntegrationServices")>]
-[<assembly: AssemblyDescriptionAttribute("SQL Server Integration Services management library for executing and working with SSIS packages")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyDescriptionAttribute("SQL Server Integration Services management library for executing and working with SSIS packages located in the integration services catalog.")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.0.1"
