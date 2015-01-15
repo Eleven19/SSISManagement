8c25a3376ebb64253edcc9a0e6ea8271995f4649
Damian Reeves <damreev@gmail.com>
2015-01-15 09:47:05 -0500

    Add some testing around the SsisApplication object

--- src/SSISManagement/ISsisApplication.cs
+++ src/SSISManagement/ISsisApplication.cs
@@ -1,12 +1,32 @@
+using System;
+
 namespace SqlServer.Management.IntegrationServices
 {
     public interface ISsisApplication
     {
-        
+        SsisConfiguration Configuration { get; }
     }
 
-    internal class SsisApplication : ISsisApplication
+    /// <summary>
+    /// The SsisApplication is the entry point into the Integration Services Management API.
+    /// </summary>
+    public class SsisApplication : ISsisApplication
     {
-        
+        private readonly SsisConfiguration _configuration;
+
+        public SsisApplication():this(new SsisConfiguration())
+        {            
+        }
+
+        public SsisApplication(SsisConfiguration configuration)
+        {
+            if (configuration == null) throw new ArgumentNullException("configuration");
+            _configuration = configuration;
+        }
+
+        public SsisConfiguration Configuration
+        {
+            get { return _configuration; }
+        }
     }
 }
---
+++ tests/SSISManagement.Tests/SsisApplicationTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+using Xbehave;
+
+namespace SqlServer.Management.IntegrationServices
+{
+    public class SsisApplicationTests
+    {
+        [Scenario]
+        public void WhenCreatingFromParameterlessConstuctor(SsisApplication application)
+        {
+            "Given a SsisApplication created from the parameterless constructor"
+                ._(() => application = new SsisApplication());
+            "Then the Configuration property should not be null"
+                ._(() => application.Configuration.Should().NotBeNull());
+        }
+
+        [Scenario]
+        public void WhenCreatingByProvidingANullConfigurationObject(SsisApplication application, Action ctorCall)
+        {
+            "Given a constructor call with a null configuration"
+                ._(() =>
+                {
+                    SsisConfiguration configuration = null;
+                    ctorCall = ()  => application = new SsisApplication(configuration);
+                });
+            "Then the constructor should throw an ArgumentNullException"
+                ._(() => ctorCall.ShouldThrow<ArgumentNullException>().Where(ex=>ex.ParamName == "configuration"));
+        }
+
+        [Scenario]
+        public void WhenCreatingByProvidingConfigurationObject(SsisApplication application, SsisConfiguration configuration)
+        {
+            "Given a SsisApplication created from the parameterless constructor"
+                ._(() =>
+                {
+                    configuration = new SsisConfiguration();
+                    application = new SsisApplication(configuration);
+                });
+            "Then the Configuration property should be the same as the passed in configuration"
+                ._(() => application.Configuration.Should().BeSameAs(configuration));
+        }
+    }
+}
