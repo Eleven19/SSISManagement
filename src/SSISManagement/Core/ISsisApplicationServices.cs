cbe50010d372f2fbbb984a7c76fdfb313b5ba660
Damian Reeves <damreev@gmail.com>
2015-01-16 16:15:26 -0500

    Working on model to provide services to the application

---
+++ src/SSISManagement/Core/ISsisApplicationServices.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SqlServer.Management.IntegrationServices.Data;
+
+namespace SqlServer.Management.IntegrationServices.Core
+{
+    public interface ISsisApplicationServices
+    {
+        IDbAccessorFactory DbAccessorFactory { get; }
+
+    }
+
+    internal class SsisApplicationServices : ISsisApplicationServices
+    {
+        private IDbAccessorFactory _dbAccessorFactory;
+
+        public SsisApplicationServices()
+        {
+            _dbAccessorFactory = new DbAccessorFactory();
+        }
+        public IDbAccessorFactory DbAccessorFactory
+        {
+            get { return _dbAccessorFactory; }
+        }
+
+        public void SetDbAccessorFactory(IDbAccessorFactory factory)
+        {
+            if (factory == null) throw new ArgumentNullException("factory");
+            _dbAccessorFactory = factory;
+        }
+    }
+
+    public class SsisApplicationServicesBuilder
+    {
+        private readonly SsisApplicationServices _applicationServices;
+
+        public SsisApplicationServicesBuilder()
+        {
+            _applicationServices = new SsisApplicationServices();
+        }
+        public ISsisApplicationServices Build()
+        {
+            return _applicationServices;
+        }
+    }
+}
